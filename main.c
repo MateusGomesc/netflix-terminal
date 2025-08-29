73d127cf71bf136158f773c8d1869f8541652541
Mateus Gomes <129867943+MateusGomesc@users.noreply.github.com>
2025-08-29 11:37:07 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,92 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include <string.h>
 #include "list.h"
+#include "stack.h"
 
-void logo(){
-    printf("======================================\n");
-    printf("|                                    |\n");
-    printf("|              %sNETFLIX%s               |\n", COR_VERMELHA, RESET_COR);
-    printf("|                                    |\n");
-    printf("======================================\n\n\n");
+void clean_buffer(){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
 
 int main()
 {
-    // Configurando terminal para UTF-8
-    setlocale(LC_ALL, "Portuguese");
-    system("chcp 65001 > nul");
+    int opc, ret;
 
-    int ret, opc;
-    char title[50];
+    // creating data structures
+    ListVerbete* l = createListVerbete();
+    Stack* s = createStack();
 
-    ListUsers* listUsers = createListUsers();
-
-    // Carregando dados dos arquivos para a lista
-    loadData(listUsers);
+    // loading verbetes file
+    loadVerbetes(l);
 
     do{
         system("cls");
-        logo();
-        printUsers(listUsers);
-        printf("Escolha do usuário: ");
+        printf("Menu -----------------------\n\n");
+        printf("[1] - Pesquisar significado\n");
+        printf("[2] - Pesquisar significado do arquivo frases.txt\n\n");
+        printf("Opcao: ");
         scanf("%d", &opc);
+        clean_buffer();
         system("cls");
-        logo();
-
-        int quantUsers = lengthUser(listUsers);
-        NodeUser* node;
 
         switch(opc){
             case 1:
-                if(quantUsers >= 1){ // Verificacao de seguranca de acordo com a quantidade de usuarios
-                    // Recebendo usuario com seguranca
-                    node = walkNextUser(listUsers, 0);
+                printf("Pesquisar significado -----------------\n\n");
+
+                char word[50];
+
+                printf("Palavra: ");
+                scanf(" %49[^\n]", word);
+                clean_buffer();
+
+                if(!findVerbete(l, word)){
+                    printf("Verbete nao encontrado!\n\n");
                 }
 
                 break;
             case 2:
-                if(quantUsers >= 2){
-                    node = walkNextUser(listUsers, 1);
-                }
-
-                break;
-            case 3:
-                if(quantUsers >= 3){
-                    node = walkNextUser(listUsers, 2);
-                }
-
-                break;
-            case 4:
-                if(quantUsers == 4){
-                    node = walkNextUser(listUsers, 3);
-                }
-
+                printf("Significados encontrados para frases.txt -----------------\n\n");
+                getMeaning(s, l);
                 break;
             default:
-                printf(COR_VERMELHA "Usuário inválido!\n\n" RESET_COR);
-                continue;
+                printf("Opcao invalida!\n\n");
         }
-
-        // Printa com seguranca todas as series do usuario
-        printSeries(node->data.series, node->data.name);
-
-        printf("Nome da série que deseja assistir: ");
-        scanf(" %49[^\n]", title);
-
-        // Abre url
-        openUrl(node->data.series, title);
 
         printf("Digite 1 para continuar e 0 para sair: ");
         scanf("%d", &ret);
+        clean_buffer();
     }while(ret);
 
-    system("cls");
-    logo();
-    printf("Volte sempre! Suas séries estarão sempre aqui na %sNETFLIX%s!\n", COR_VERMELHA, RESET_COR);
-
-    freeListUsers(listUsers);
+    // free data structures of memory
+    freeListVerbete(l);
+    freeStack(s);
 
     return 0;
 }
